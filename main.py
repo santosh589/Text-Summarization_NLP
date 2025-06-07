c29588e97914fd430fd0bea97cb9b0d35be209e4
Santoshhr159 <santoshhr159@gmail.com>
2025-06-07 22:01:04 +0530

    All Pipeline  completed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from TextSummarizer.pipeline.data_ingestion_1 import DataIngestionPipeline
 from TextSummarizer.pipeline.data_validation_2 import DataValidationTrainingPipeline
+from TextSummarizer.pipeline.data_transformation_3 import DataTransformationPipeline
+from TextSummarizer.pipeline.model_trainer_4 import ModelTrainerTrainingPipeline
+from TextSummarizer.pipeline.model_evaluation_5 import ModelEvaluationTrainingPipeline
 from TextSummarizer.logging import logger
 
 Step_name = "Data Ingestion step"
@@ -20,6 +23,44 @@ try:
     logger.info(f"<<< Step {Step_name} start >>>\n\n\n ")
     data_validation= DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f"<<< Step {Step_name} completed >>\n\n>")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+Step_name = "Data Transformation step"
+
+try:
+    logger.info(f"<<< Step {Step_name} start >>>\n\n\n ")
+    data_validation= DataTransformationPipeline()
+    data_validation.main()
+    logger.info(f"<<< Step {Step_name} completed >>\n\n>")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+Step_name = "Model Training step"
+
+try:
+    logger.info(f"<<< Step {Step_name} start >>>\n\n\n ")
+    model_training= ModelTrainerTrainingPipeline()
+    model_training.main()
+    logger.info(f"<<< Step {Step_name} completed >>\n\n>")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+Step_name = "Model Evaluation step"
+
+try:
+    logger.info(f"<<< Step {Step_name} start >>>\n\n\n ")
+    model_evaluation= ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f"<<< Step {Step_name} completed >>\n\n>")
 
 except Exception as e:
--- src/TextSummarizer/components/data_validation.py
+++ src/TextSummarizer/components/data_validation.py
@@ -12,7 +12,7 @@ class DataValidation:
         try:
             validation_status= None
 
-            all_files = os.listdir(os.path.join("artifacts", "data_ingestion", "Dataset", "samsum_dataset"))
+            all_files = os.listdir(os.path.join("artifacts", "data_ingestion", "samsum_dataset", "samsum_dataset"))
 
             for file in all_files:
                 if file not in self.config.ALL_REQUIRED_FILES:
